22461cf1b058c8ca2fec70fdf3a85492853c72e1
Muataz Attaia <moatazataya346@gmail.com>
2023-03-12 10:52:46 +0300

    expanded database added membership to customer added order table

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Category(models.Model):
     name = models.CharField(max_length=15)
 
@@ -22,8 +24,22 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
 
 class Customer(models.Model):
+    BRONZE = 'B'
+    GOLD = 'G'
+    SILVER = 'S'
+    MEMBERSHIP_CHOICES = [(GOLD,"Gold"),(SILVER,"Silver"),(BRONZE,"Bronze")]
     first_name = models.CharField(max_length =20, blank=False, null=True)
     last_name = models.CharField(max_length=20, blank=False, null=True)
     email = models.EmailField(blank=False, null=True, unique=True)
     birth_date = models.DateField(auto_now_add=True,null=True)
     phone = models.IntegerField(max_length=8,blank=True)
+    membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,
+                 default=BRONZE)
+
+class Order(models.Model):
+    PENDING_STATUS = 'P'
+    COMPLETE_STATUS = 'C'
+    FAILED_STATUS = 'F'
+    STATUS_CHOICES = [(PENDING_STATUS,"Pending"),(COMPLETE_STATUS,"Complete"),(FAILED_STATUS,"Failed")]
+    placed_at = models.DateField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices= STATUS_CHOICES)
