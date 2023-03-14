5e19479503f42010b43523e694d3ac98b4828ac3
Muataz Attaia <moatazataya346@gmail.com>
2023-03-14 10:52:14 +0300

    expanded store app database fields

--- store/models.py
+++ store/models.py
@@ -6,22 +6,27 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=15)
+    featured_product = models.OneToOneField('Product', 
+                        on_delete=models.SET_NULL, null=True,
+                        related_name='+') 
+                        # '+' will cancel the reverse relationship
 
 class Product(models.Model):
     title = models.CharField(max_length=255, blank=False, null=True)
     description = models.TextField(max_length=1000, blank=True, null=True)
-    price = models.DecimalField(blank=False, null=True)
-    inventory = models.IntegerField(blank=False, null=True)
+    price = models.DecimalField(decimal_places=3,blank=False, null=True,max_digits=6)
+    inventory = models.PositiveIntegerField(blank=False, null=True)
     last_update = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, blank=True, null=False)
+    category = models.ForeignKey(Category, blank=True, null=True,on_delete=models.SET_NULL)
+    slug = models.SlugField()
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, blank=False, null=True)
-    product = models.ForeignKey(Product, blank=True, null=True)
-    quantity = models.IntegerField()
+    cart = models.ForeignKey(Cart, blank=False, null=True,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, blank=True, null=True,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(blank=False)
 
 class Customer(models.Model):
     BRONZE = 'B'
@@ -32,7 +37,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=20, blank=False, null=True)
     email = models.EmailField(blank=False, null=True, unique=True)
     birth_date = models.DateField(auto_now_add=True,null=True)
-    phone = models.IntegerField(max_length=8,blank=True)
+    phone = models.PositiveIntegerField(max_length=8,blank=True)
     membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,
                  default=BRONZE)
 
@@ -43,3 +48,11 @@ class Order(models.Model):
     STATUS_CHOICES = [(PENDING_STATUS,"Pending"),(COMPLETE_STATUS,"Complete"),(FAILED_STATUS,"Failed")]
     placed_at = models.DateField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices= STATUS_CHOICES)
+    
+    
+
+class OrderItem(models.Model):
+    quantity = models.PositiveIntegerField(blank=False)
+    unit_price = models.DecimalField(decimal_places=3,blank=False,max_digits=6)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    order= models.ForeignKey(Order, on_delete=models.CASCADE)
