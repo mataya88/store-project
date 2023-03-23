b122ccb59eea91029a966781a58982e31b8d5446
Muataz Attaia <moatazataya346@gmail.com>
2023-03-23 10:51:53 +0300

    updated home html to get a queryset

--- playarea/views.py
+++ playarea/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models import Q
+from store.models import Product
 # Create your views here.
 
+
+
 def say_hello(request):
-    return render(request, 'playarea/home.html',{'name':"Muataz"})
+    queryset = Product.objects.filter(unit_price__lt=5)
+
+    return render(request, 
+    'playarea/home.html',
+    {'name':"Muataz",
+    "products":list(queryset)})
 
 def return_page(request):
     return render(request,'playarea/new.html',{'phone':99840823})
