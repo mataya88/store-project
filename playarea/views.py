3aa2006f93e509877810c30b67babbc5b9869df0
Muataz Attaia <moatazataya346@gmail.com>
2023-04-04 10:55:48 +0300

    added queries to say_hello view

--- playarea/views.py
+++ playarea/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
-from store.models import Product
+from django.db.models import Q,F
+from store.models import *
+from django.db.models.aggregates import Count, Sum, Min, Max, Avg
+
 # Create your views here.
 
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(unit_price__lt=5)
-
+    # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    result = Product.objects.aggregate(count = Count('pk'), min_price = Min('unit_price'))
     return render(request, 
     'playarea/home.html',
     {'name':"Muataz",
-    "products":list(queryset)})
+    "products":[],
+    'result':result})
 
 def return_page(request):
     return render(request,'playarea/new.html',{'phone':99840823})
